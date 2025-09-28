ffcb15ed6b74fe803067b5b5cf44dc5790db8168
mark <markoc1120@gmail.com>
2025-09-28 15:31:05 +0200

    feat: now generating new files for every thumbnail updates

--- assets.go
+++ assets.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
-
-	"github.com/google/uuid"
 )
 
 func (cfg apiConfig) ensureAssetsDir() error {
@@ -16,9 +16,13 @@ func (cfg apiConfig) ensureAssetsDir() error {
 	return nil
 }
 
-func getAssetPath(videoID uuid.UUID, mediaType string) string {
+func getAssetPath(mediaType string) string {
+	randomKey := make([]byte, 32)
+	rand.Read(randomKey)
+	randomName := base64.RawURLEncoding.EncodeToString(randomKey)
+
 	ext := mediaTypeExt(mediaType)
-	return fmt.Sprintf("%s%s", videoID.String(), ext)
+	return fmt.Sprintf("%s%s", randomName, ext)
 }
 
 func (cfg apiConfig) getAssetDiskPath(assetPath string) string {
